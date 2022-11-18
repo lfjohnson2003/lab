0e814a70172a9813fe92f152ae8837e5a92c0d87
lfjohnson2003 <118492209+lfjohnson2003@users.noreply.github.com>
2022-11-18 13:06:25 -0600

    Added the docstring and type hinting

--- account.py
+++ account.py
@@ -4,14 +4,26 @@ class Account:
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount) -> bool:
+        """
+        A method that deposits the amount into account as long as the amount is greater than 0.
+        :param amount: The amount of money to be deposited to the account.
+        :return: Returns a Boolean, False if depositing a negative amount, and returns True if greater than 0.
+        """
         if amount <= 0:
             return False
         else:
             self.__account_balance += amount
             return True
 
-    def withdraw(self, amount):
+    def withdraw(self, amount) -> bool:
+        """
+        A method that withdraws the amount from the account as long as the amount is greater than 0 but not more
+        than the amount that is already in the account.
+        :param amount: The amount of money to be withdrawn from the account.
+        :return: Returns a Boolean, False if withdrawing a negative amount or an amount greater than what's in the
+        account, and returns True if greater than 0 and less than the amount in the account.
+        """
         if amount <= 0 or amount > self.__account_balance:
             return False
         else:
